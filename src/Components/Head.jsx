056416328cf8dc82471480bbff3d04f50d0afcd0
SabirHossen339 <sabirshimanto01729@gmail.com>
2026-02-10 10:32:21 +0600

    Added Date FNS and Customized theme

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 const Head = () => {
   return (
     <div className='flex justify-center flex-col items-center gap-2'>
-      <img src={logo} alt="" />
-      <p>Journalism Without Fear or Favour</p>
+      <img className="w-112.5" src={logo} alt="" />
+      <p className='text-accent'>Journalism Without Fear or Favour</p>
+      <p className='text-semibold text-accent'>{format(new Date(), "EEEE, MMMM MM, 	yyyy") }</p>
     </div>
   );
 };
